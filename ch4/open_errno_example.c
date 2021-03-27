c11e4f5ca6f3340d9f7e4318dd88d908b0a356b9
leopardshadow <thurtu811@gmail.com>
2021-03-27 20:34:32 +0800

    update file open errno

--- ch4/open_errno_example.c
+++ ch4/open_errno_example.c
@@ -6,17 +6,20 @@
 int main(int argc, char const *argv[])
 {
     // errno is defined somewhere else
-    errno = 0;
+    // errno = 0;
 
     int fd = open("a_file_doesn't_exist", O_RDONLY);
 
     // since the file doesn't exist, file descriptor will retuen -1
     printf("%d\n", fd);
 
-    // however, there might be many possibilities for fd = -1
+    // always check the result beefore obtaining errno value!
+    // there might be many possibilities for fd = -1
     // print the errno value
-    printf("%d\n", errno);
-    fprintf( stderr, "system error: %s\n" , sys_errlist[errno] ); 
+    if(fd == -1) {
+        printf("%d\n", errno);
+        fprintf( stderr, "system error: %s\n" , sys_errlist[errno] ); 
+    }
 
     return 0;
 }
