ff6ef4d3d1f3a335c67dcf0ba41dea1e1c9b40cd
leopardshadow <thurtu811@gmail.com>
2021-03-27 12:21:54 +0800

    add 4-1: the command tee

---
+++ ch4/c4_1.c
@@ -0,0 +1,38 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define N 2
+
+int main(int argc, char const *argv[])
+{
+    char buf[N];
+    ssize_t rdNum;
+    int openFlag = O_WRONLY | O_CREAT | O_TRUNC;
+    mode_t filePerms = S_IRUSR | S_IWUSR;
+
+    int fd = open(argv[1], openFlag, filePerms);
+    // printf("%d\n", fd);
+    if(fd == -1) {
+        _exit(-1);
+    }
+
+    for(;;) {
+        rdNum = read(STDIN_FILENO, buf, N);
+        // printf(" rd = %zu ", rdNum);
+
+        if(rdNum == -1)
+            _exit(-1);
+        
+        write(fd, buf, rdNum);
+        write(STDOUT_FILENO, buf, rdNum);
+
+        if(buf[rdNum-1]=='\n')
+            break;
+    }
+
+    close(fd);
+
+    return 0;
+}
