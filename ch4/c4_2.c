5ddda6f3d8bf8006fce12b7e34983ffd149dd60b
leopardshadow <thurtu811@gmail.com>
2021-04-07 23:31:55 +0800

    add 4-2

--- ch4/c4_2.c
+++ ch4/c4_2.c
@@ -1,23 +1,38 @@
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define BUF_SIZE 100000
+#define BUF_SIZE 1000
 
 int main(int argc, char const *argv[])
 {
-    int openFlag = O_RDONLY ;
-    int fd = open("holes", openFlag);
-    if(fd == -1) {
+    if(argc != 3) {
+        fprintf(stderr, "wrong arguments number\r\n");
+    }
+
+    int fdRead = open(argv[1], O_RDONLY);
+    int fdWrite = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+
+    if(fdRead == -1) {
+        fprintf(stderr, "fail to open file %s\r\n", argv[1]);
         _exit(-1);
     }
 
+    if(fdWrite == -1) {
+        fprintf(stderr, "fail to open file %s\r\n", argv[2]);
+        _exit(-1);
+    }
+
+
     char buf[BUF_SIZE];
+    int numRead;
 
-    printf("%lu", read(fd, buf, BUF_SIZE));
+    while((numRead = read(fdRead, buf, BUF_SIZE)) > 0) {
+        write(fdWrite, buf, numRead);
+    }
 
-    close(fd);
+    close(fdRead);
+    close(fdWrite);
 
     return 0;
 }
