d897ce52e6ec59a6e2675159aaa829773014d86b
leopardshadow <thurtu811@gmail.com>
2021-04-08 18:55:37 +0800

    update ch6 test.c

--- ch6/test.c
+++ ch6/test.c
@@ -6,8 +6,8 @@
 */
 
 /*
-__TEXT	__DATA	__OBJC	others	dec	hex
-4096	0	    0	    4294971392	4294975488	100002000
+   text	   data	    bss	    dec	    hex	filename
+   1418	    544	      8	   1970	    7b2	a.out
 */
 // int main(int argc, char const *argv[])
 // {
@@ -16,8 +16,8 @@ __TEXT	__DATA	__OBJC	others	dec	hex
 
 
 /*
-__TEXT	__DATA	__OBJC	others	dec	hex
-4096	4096	0	4294971392	4294979584	100003000
+   text	   data	    bss	    dec	    hex	filename
+   1571	    600	      8	   2179	    883	a.out
 */
 // int main(int argc, char const *argv[])
 // {
@@ -26,9 +26,24 @@ __TEXT	__DATA	__OBJC	others	dec	hex
 // }
 
 
-int a[30];
+/*
+   text	   data	    bss	    dec	    hex	filename
+   1655	    600	      8	   2263	    8d7	a.out
+*/
 int main(int argc, char const *argv[])
 {
-	char *p = malloc(sizeof(char)*100);
+	char c[100000];	
 	return 0;
 }
+
+
+
+/*
+   text	   data	    bss	    dec	    hex	filename
+   1418	    544	    152	   2114	    842	a.out
+*/
+// int a[30];
+// int main(int argc, char const *argv[])
+// {
+// 	return 0;
+// }
