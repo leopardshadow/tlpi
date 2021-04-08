47a7fe539a9740fca5254956f8566d75c8253b27
leopardshadow <thurtu811@gmail.com>
2021-04-08 18:57:36 +0800

    update ch6 test.c

--- ch6/test.c
+++ ch6/test.c
@@ -30,11 +30,11 @@
    text	   data	    bss	    dec	    hex	filename
    1655	    600	      8	   2263	    8d7	a.out
 */
-int main(int argc, char const *argv[])
-{
-	char c[100000];	
-	return 0;
-}
+// int main(int argc, char const *argv[])
+// {
+// 	char c[100000];	
+// 	return 0;
+// }
 
 
 
@@ -47,3 +47,13 @@ int main(int argc, char const *argv[])
 // {
 // 	return 0;
 // }
+
+/*
+   text	   data	    bss	    dec	    hex	filename
+   1418	    544	      8	   1970	    7b2	a.out
+*/
+int main(int argc, char const *argv[])
+{
+	int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+	return 0;
+}
